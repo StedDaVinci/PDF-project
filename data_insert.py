6ba39cb6228a2981c73630f135cce9686abe7dac
Bugra2006 <143710278+Bugra2006@users.noreply.github.com>
2024-04-19 10:17:53 +0200

    fintsh

--- data_insert.py
+++ data_insert.py
@@ -11,24 +11,29 @@ def insert_data(json_data, c, conn):
     factuur = json_data['factuur']
     klant = factuur['klant']
 
+    c.execute('CREATE TABLE if not exists klanten (klant_id INTEGER PRIMARY KEY,naam TEXT,adres TEXT,postcode TEXT,stad TEXT,KVK_nummer TEXT);')
+    c.execute('CREATE TABLE if not exists facturen (factuur_id INTEGER PRIMARY KEY,factuurnummer TEXT,factuurdatum DATE,betaaltermijn TEXT,klant_id INTEGER,FOREIGN KEY (klant_id) REFERENCES klanten(klant_id));')
+    c.execute('CREATE TABLE if not exists factuurregels (regel_id INTEGER PRIMARY KEY,factuur_id INTEGER,productnaam TEXT,aantal INTEGER,prijs_per_stuk_excl_btw REAL,FOREIGN KEY (factuur_id) REFERENCES facturen(factuur_id));')
+    
+    
     # Klantgegevens invoeren
-    c.execute("INSERT INTO klanten (naam, adres, postcode, stad, KVK-nummer) VALUES (?, ?, ?, ?, ?)",
+    c.execute("INSERT INTO klanten (naam, adres, postcode, stad, KVK_nummer) VALUES (?, ?, ?, ?, ?)",
               (klant['naam'], klant['adres'], klant['postcode'], klant['stad'], klant['KVK-nummer']))
     conn.commit()
 
     klant_id = c.lastrowid  # Het ID van de net ingevoerde klant
 
     # Factuurgegevens invoeren
-    c.execute("INSERT INTO facturen (ordernummer, orderdatum, betaaltermijn, klant_id) VALUES (?, ?, ?, ?)",
-              (factuur['ordernummer'], factuur['orderdatum'], factuur['betaaltermijn'], klant_id))
+    c.execute("INSERT INTO facturen (factuurnummer, factuurdatum, betaaltermijn, klant_id) VALUES (?, ?, ?, ?)",
+              (factuur['factuurnummer'], factuur['factuurdatum'], factuur['betaaltermijn'], klant_id))
     conn.commit()
 
     factuur_id = c.lastrowid  # Het ID van de net ingevoerde factuur
 
     # Factuurregels invoeren
     for product in factuur['producten']:
-        c.execute("INSERT INTO factuurregels (factuur_id, productnaam, aantal, prijs_per_stuk_excl_btw, btw_percentage) VALUES (?, ?, ?, ?, ?)",
-                  (factuur_id, product['productnaam'], product['aantal'], product['prijs_per_stuk_excl_btw'], product['btw_percentage']))
+        c.execute("INSERT INTO factuurregels (factuur_id, productnaam, aantal, prijs_per_stuk_excl_btw ) VALUES (?, ?, ?, ?)",
+                  (factuur_id, product['productnaam'], product['aantal'], product['prijs_per_stuk_excl_btw']))
         conn.commit()
 
 # Loop door alle JSON-bestanden in de map
